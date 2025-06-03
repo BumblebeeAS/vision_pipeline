81c6a684c7e058517f90cb5bb78b815aa671b39a
SamuelFoo <fooenzesamuel@gmail.com>
2025-06-03 12:58:38 +0800

    Add shark_fish estimator node to gate launch

--- launch/gate_yolo.launch.py
+++ launch/gate_yolo.launch.py
@@ -1,10 +1,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node, PushRosNamespace
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, PushRosNamespace
 
 
 def generate_launch_description():
@@ -40,6 +41,12 @@ def generate_launch_description():
             package="pose_estimator",
             executable="gate_pose_estimator_node",
             name="gate_pose_estimator_node",
+            parameters=[config],
+        ),
+        Node(
+            package="pose_estimator",
+            executable="shark_fish_estimator_node",
+            name="gate_shark_fish_estimator_node",
             parameters=[config],
         ),
     ]
