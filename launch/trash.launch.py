282de4f6687483cd5345c8dc1fe062a73ecbe415
SamuelFoo <fooenzesamuel@gmail.com>
2025-06-18 12:54:05 +0800

    Add trash launch

---
+++ launch/trash.launch.py
@@ -0,0 +1,81 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import LifecycleNode, Node, PushRosNamespace
+
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("vision_pipeline"),
+        "config",
+        "auv4_orin",
+        "trash.yaml",
+    )
+
+    launch_objects = [PushRosNamespace("/auv4/trash")]
+
+    pipeline_nodes = [
+        LifecycleNode(
+            package="yolo_ros_trt",
+            executable="yolo_node",
+            name="trash_yolo_node",
+            parameters=[config],
+            namespace="",
+        ),
+        LifecycleNode(
+            package="yolo_ros_trt",
+            executable="yolo_node",
+            name="symbol_yolo_node",
+            parameters=[config],
+            namespace="",
+        ),
+        Node(
+            package="pose_estimator",
+            executable="trash_pose_estimator_node",
+            name="trash_pose_estimator_node",
+            parameters=[config],
+            namespace="trash",
+        ),
+        # Node(
+        #     package="pose_estimator",
+        #     executable="shark_fish_estimator_node",
+        #     name="gate_shark_fish_estimator_node",
+        #     parameters=[config],
+        # ),
+        Node(
+            package="vision_pipeline",
+            executable="lifecycle_manager",
+            name="trash_lifecycle_manager",
+            parameters=[config],
+        ),
+    ]
+
+    repub_nodes = [
+        Node(
+            package="image_transport",
+            executable="republish",
+            name="trash_compression_node",
+            arguments=["raw", "compressed"],
+            output="screen",
+            parameters=[{"out.jpeg_quality": 50}],
+            remappings=[
+                ("in", "trash/yolo/image"),
+                ("out/compressed", "trash/yolo/image/compressed"),
+            ],
+        ),
+        Node(
+            package="image_transport",
+            executable="republish",
+            name="symbol_compression_node",
+            arguments=["raw", "compressed"],
+            output="screen",
+            parameters=[{"out.jpeg_quality": 50}],
+            remappings=[
+                ("in", "symbol/yolo/image"),
+                ("out/compressed", "symbol/yolo/image/compressed"),
+            ],
+        ),
+    ]
+
+    return LaunchDescription(launch_objects + pipeline_nodes + repub_nodes)
