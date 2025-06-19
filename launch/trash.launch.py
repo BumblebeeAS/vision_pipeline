4d40d0d19652c302b6c46e0018f0b2bb9f23ec06
SamuelFoo <fooenzesamuel@gmail.com>
2025-06-20 02:33:10 +0800

    Update trash launch

--- launch/trash.launch.py
+++ launch/trash.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import LifecycleNode, Node, PushRosNamespace
+from launch_ros.actions import Node, PushRosNamespace
 
 
 def generate_launch_description():
@@ -16,26 +16,29 @@ def generate_launch_description():
     launch_objects = [PushRosNamespace("/auv4/trash")]
 
     pipeline_nodes = [
-        LifecycleNode(
+        Node(
             package="yolo_ros_trt",
             executable="yolo_node",
             name="trash_yolo_node",
             parameters=[config],
-            namespace="",
         ),
-        LifecycleNode(
+        Node(
             package="yolo_ros_trt",
             executable="yolo_node",
             name="symbol_yolo_node",
             parameters=[config],
-            namespace="",
+        ),
+        Node(
+            package="pose_estimator",
+            executable="trash_table_pose_estimator_node",
+            name="trash_table_pose_estimator_node",
+            parameters=[config],
         ),
         Node(
             package="pose_estimator",
             executable="trash_pose_estimator_node",
             name="trash_pose_estimator_node",
             parameters=[config],
-            namespace="trash",
         ),
         # Node(
         #     package="pose_estimator",
@@ -45,8 +48,8 @@ def generate_launch_description():
         # ),
         Node(
             package="vision_pipeline",
-            executable="lifecycle_manager",
-            name="trash_lifecycle_manager",
+            executable="lifecycle_manager_node",
+            name="trash_lifecycle_manager_node",
             parameters=[config],
         ),
     ]
