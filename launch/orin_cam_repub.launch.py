03566892e8378b3b5a4c231a867017b722d41448
SamuelFoo <fooenzesamuel@gmail.com>
2025-08-08 22:54:19 +0800

    Minor refactor for orin_cam_repub

--- launch/orin_cam_repub.launch.py
+++ launch/orin_cam_repub.launch.py
@@ -7,47 +7,47 @@ def generate_launch_description():
     repub_nodes = []
 
     for cam_name in ["front_cam", "bot_cam"]:
-        repub_node = ComposableNode(
-            package="custom_image_republisher",
-            plugin="custom_image_republisher::Republisher",
-            name="sbc2orin_repub_node",
-            parameters=[{"in_transport": "compressed", "out_transport": "raw"}],
-            namespace=f"auv4/{cam_name}/",
-            remappings=[
-                ("in/compressed", "color/image/compressed"),
-                ("out", "color/image/orin"),
-            ],
-        )
-
-        # Lossy compression for visualization
-        # (SBC -> Orin is at default 95% quality)
-        compress_node = ComposableNode(
-            package="custom_image_republisher",
-            plugin="custom_image_republisher::Republisher",
-            name="orin_compression_node",
-            parameters=[
-                {
-                    "in_transport": "raw",
-                    "out_transport": "compressed",
-                    ".out.jpeg_quality": 50,
-                }
-            ],
-            namespace=f"auv4/{cam_name}/",
-            remappings=[
-                ("in", "color/image/orin"),
-                ("out/compressed", "color/vis/image/compressed"),
-            ],
-        )
+        cam_container_nodes = [
+            ComposableNode(
+                package="custom_image_republisher",
+                plugin="custom_image_republisher::Republisher",
+                name="sbc2orin_repub_node",
+                parameters=[{"in_transport": "compressed", "out_transport": "raw"}],
+                namespace=f"auv4/{cam_name}/",
+                remappings=[
+                    ("in/compressed", "color/image/compressed"),
+                    ("out", "color/image/orin"),
+                ],
+            ),
+            # Lossy compression for visualization
+            # (SBC -> Orin is at default 95% quality)
+            ComposableNode(
+                package="custom_image_republisher",
+                plugin="custom_image_republisher::Republisher",
+                name="orin_compression_node",
+                parameters=[
+                    {
+                        "in_transport": "raw",
+                        "out_transport": "compressed",
+                        ".out.jpeg_quality": 50,
+                    }
+                ],
+                namespace=f"auv4/{cam_name}/",
+                remappings=[
+                    ("in", "color/image/orin"),
+                    ("out/compressed", "color/vis/image/compressed"),
+                ],
+            ),
+        ]
 
         container = ComposableNodeContainer(
             name="orin_repub_container",
             namespace=f"auv4/{cam_name}/",
             package="rclcpp_components",
             executable="component_container",
-            composable_node_descriptions=[repub_node, compress_node],
+            composable_node_descriptions=cam_container_nodes,
             output="screen",
         )
-
         repub_nodes.append(container)
 
     return LaunchDescription(repub_nodes)
