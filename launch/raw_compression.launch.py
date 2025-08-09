3b20a23ffd0e213c02989df8eac481e37101d216
xelisce <xelisce@gmail.com>
2025-08-09 13:35:56 +0800

    Update miniauv config and raw_compression.launch.py

---
+++ launch/raw_compression.launch.py
@@ -0,0 +1,72 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, PushRosNamespace
+
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("vision_pipeline"),
+        "config",
+        "auv4_orin",
+        "image_matching.yaml",
+    )
+
+    launch_objects = [
+        DeclareLaunchArgument(
+            "camera_name",
+            default_value="front_cam",
+        ),
+        PushRosNamespace(["/auv4/", LaunchConfiguration("camera_name")]),
+    ]
+
+    pipeline_nodes = [
+        Node(
+            package="image_processing",
+            executable="image_brighten_node",
+            name="image_brighten_node",
+            parameters=[config],
+        ),
+        Node(
+            package="image_matching",
+            executable="simple_matcher_node",
+            name="simple_matcher_node",
+            parameters=[config],
+        ),
+        Node(
+            package="pose_estimator",
+            executable="points_pose_estimator_node",
+            name="points_pose_estimator_node",
+            parameters=[config],
+        ),
+    ]
+
+    repub_nodes = [
+        Node(
+            package="custom_image_republisher",
+            executable="republish",
+            name="brighten_compression_node",
+            arguments=["raw", "compressed"],
+            output="screen",
+            remappings=[
+                ("in", "color/brighten/image"),
+                ("out/compressed", "color/brighten/image/compressed"),
+            ],
+        ),
+        Node(
+            package="custom_image_republisher",
+            executable="republish",
+            name="image_matching_compression_node",
+            arguments=["raw", "compressed"],
+            output="screen",
+            remappings=[
+                ("in", "image_matching/image"),
+                ("out/compressed", "image_matching/image/compressed"),
+            ],
+        ),
+    ]
+
+    return LaunchDescription(launch_objects + pipeline_nodes + repub_nodes)
