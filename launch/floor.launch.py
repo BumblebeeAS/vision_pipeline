61626056d0384759c5a0417b385e30330e55b96c
SamuelFoo <fooenzesamuel@gmail.com>
2025-12-10 23:56:48 +0800

    Add floor launch

---
+++ launch/floor.launch.py
@@ -0,0 +1,43 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node, PushRosNamespace
+
+
+def generate_launch_description():
+    # We can use the same config for both front and back, unused configs will be ignored
+    config = os.path.join(
+        get_package_share_directory("vision_pipeline"),
+        "config",
+        "auv4_orin",
+        "floor.yaml",
+    )
+
+    launch_objects = [PushRosNamespace("/auv4/floor")]
+
+    pipeline_nodes = [
+        Node(
+            package="yolo_ros_trt",
+            executable="yolo_node",
+            name="floor_yolo_node",
+            parameters=[config],
+        ),
+        Node(
+            package="pose_estimator",
+            executable="floor_pose_estimator_node",
+            name="floor_pose_estimator_node",
+            parameters=[config],
+        ),
+        Node(
+            package="vision_pipeline",
+            executable="lifecycle_manager_node",
+            name="lifecycle_manager_node",
+            output="screen",
+            parameters=[config, {"node_names": ["floor_yolo_node"]}],
+        ),
+    ]
+
+    vis_nodes = []
+
+    return LaunchDescription(launch_objects + pipeline_nodes + vis_nodes)
