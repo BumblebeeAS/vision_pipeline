484ad05d8fc739289f69d760ba1c9ddaf2590e91
SamuelFoo <fooenzesamuel@gmail.com>
2025-12-19 20:58:46 +0800

    Add restamp camera node for timestamp syncing with odom

--- launch/robotx/uav2_sim_image_proc.launch.py
+++ launch/robotx/uav2_sim_image_proc.launch.py
@@ -1,12 +1,26 @@
 from launch import LaunchDescription
 from launch.actions import OpaqueFunction
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer, Node
 
 from vision_pipeline.common.uav2_image_proc import get_image_proc_nodes
 
 
 def evaluate_launch(context, *args, **kwargs):
+    restamp_camera_node = Node(
+        package="image_processing",
+        executable="restamp_camera_node",
+        name="uav2_restamp_camera_node",
+        parameters=[
+            {
+                "in_image": "unstamped/image",
+                "out_image": "image",
+                "in_info": "unstamped/camera_info",
+                "out_info": "camera_info",
+            }
+        ],
+        output="screen",
+    )
+
     image_proc_container = ComposableNodeContainer(
         name="image_proc_container",
         package="rclcpp_components",
@@ -17,7 +31,7 @@ def evaluate_launch(context, *args, **kwargs):
         arguments=["--ros-args", "--log-level", "info"],
     )
 
-    return [image_proc_container]
+    return [image_proc_container, restamp_camera_node]
 
 
 def generate_launch_description():
