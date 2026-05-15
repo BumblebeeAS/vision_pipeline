fe22bc36bb9a60c9cbcdf10747d00007629441ac
bbauv3 <bumblebeeauv@gmail.com>
2026-05-15 20:35:09 +0700

    fix: remove far est

--- launch/torpedo.launch.py
+++ launch/torpedo.launch.py
@@ -23,10 +23,23 @@ def generate_launch_description():
             parameters=[config],
             namespace="",
         ),
+        LifecycleNode(
+            package="yolo_ros_trt",
+            executable="yolo_node",
+            name="torpedo_hole_yolo_node",
+            parameters=[config],
+            namespace="",
+        ),
         Node(
             package="pose_estimator",
             executable="torpedo_pose_estimator_node",
             name="torpedo_pose_estimator_node",
+            parameters=[config],
+        ),
+        Node(
+            package="pose_estimator",
+            executable="torpedo_pose_estimator_node",
+            name="torpedo_hole_pose_estimator_node",
             parameters=[config],
         ),
         Node(
@@ -37,29 +50,27 @@ def generate_launch_description():
         ),
     ]
 
-    image_matching_nodes = [
-        Node(
-            package="image_processing",
-            executable="image_brighten_node",
-            name="image_brighten_node",
-            parameters=[config],
-        ),
-        Node(
-            package="image_matching",
-            executable="simple_matcher_node",
-            name="simple_matcher_node",
-            parameters=[config],
-        ),
-        Node(
-            package="pose_estimator",
-            executable="points_pose_estimator_node",
-            name="points_pose_estimator_node",
-            parameters=[config],
-        ),
-    ]
+    # image_matching_nodes = [
+    #     Node(
+    #         package="image_processing",
+    #         executable="image_brighten_node",
+    #         name="image_brighten_node",
+    #         parameters=[config],
+    #     ),
+    #     Node(
+    #         package="image_matching",
+    #         executable="simple_matcher_node",
+    #         name="simple_matcher_node",
+    #         parameters=[config],
+    #     ),
+    #     Node(
+    #         package="pose_estimator",
+    #         executable="points_pose_estimator_node",
+    #         name="points_pose_estimator_node",
+    #         parameters=[config],
+    #     ),
+    # ]
 
     vis_nodes = []
 
-    return LaunchDescription(
-        launch_objects + yolo_nodes + image_matching_nodes + vis_nodes
-    )
+    return LaunchDescription(launch_objects + yolo_nodes + vis_nodes)
