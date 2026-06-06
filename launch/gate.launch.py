1c808d8bf8a444a84dfcb27490ef2e8b42635045
wesley <wesleywong08@gmail.com>
2026-06-06 10:38:36 +0700

    feat: update gate launch to use rs26 pose est

--- launch/gate.launch.py
+++ launch/gate.launch.py
@@ -1,9 +1,8 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node, PushRosNamespace
-
 from launch import LaunchDescription
+from launch_ros.actions import Node, PushRosNamespace
 
 
 def generate_launch_description():
@@ -44,8 +43,8 @@ def generate_launch_description():
         ),
         Node(
             package="pose_estimator",
-            executable="shark_fish_estimator_node",
-            name="gate_shark_fish_estimator_node",
+            executable="sos_repair_estimator_node",
+            name="sos_repair_estimator_node",
             parameters=[config],
         ),
         Node(
@@ -53,10 +52,7 @@ def generate_launch_description():
             executable="lifecycle_manager_node",
             name="lifecycle_manager_node",
             output="screen",
-            parameters=[
-                config,
-                {"node_names": ["gate_yolo_node", "symbol_yolo_node"]},
-            ],
+            parameters=[config],
         ),
     ]
 
